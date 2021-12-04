2016031914fe793f3bb69ef474b639b448b16e80
anasty223 <anasty22@gmail.com>
2021-12-04 21:38:28 +0200

    ошибка

---
+++ fetchCountries.js
@@ -0,0 +1,17 @@
+ 
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+    return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+}
+
+//  fetch('https://restcountries.com/v2/name?fields=name,capital,population,flags,languages')
+// .then(response => {
+//     return response.json();})
+// .then(country => {
+//     console.log(country)
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,56 @@
 import './css/styles.css';
+import countryTpl from '../tamplates/country.hbs';
+import countryOne from '../tamplates/countryOne.hbs';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from '../fetchCountries.js';
+
+
+ const refs = {
+ searchForm: document.querySelector('input#search-box'),
+ countryList: document.querySelector('.country-list'),
+ container: document.querySelector('.country-info'),
+ }
 
 const DEBOUNCE_DELAY = 300;
+refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
 
-const r = fetch('https://restcountries.com/v3.1/all');
-console.log(r)
+function onSearch(evt) {
+  evt.preventDefault();
+  const form = evt.target.value;
+  const inputValue = form.trim();
+  
+   if (inputValue === '') {
+    refs.countryList.innerHTML = '';
+    refs.container.innerHTML = '';
+     return;
+   }
+
+ fetchCountries(inputValue).then(onSuccses).catch(onError);
+
+}
+
+function onSuccses(name) {
+  console.log('eto then')
+  const markupList = countryOne(name);
+  const markupOneCountries = countryTpl(name);
+  refs.countryList.innerHTML = markupList;
+
+  if (name.length > 10) {
+    return Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.')
+
+  }
+   else if (name.length >= 2 && name.length <= 10) {
+
+    refs.container.innerHTML = countryTpl;
+  }
+  else if (name.length === 1) {
+   refs.container.innerHTML = markupOneCountries;
+    refs.countryList.innerHTML = '';
+   } 
+}
+function onError(error) {
+  Notiflix.Notify.failure('Oops, there is no country with that name')
+  refs.container.innerHTML = '';
+  
+}
